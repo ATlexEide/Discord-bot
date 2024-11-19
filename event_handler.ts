e0fa9974fb5fdda1fc867203468b2032b11718af
Alexander Eide <atlexeide@gmail.com>
2024-11-19 10:44:55 +0100

    Add getEmbed function

--- event_handler.ts
+++ event_handler.ts
@@ -1,15 +1,24 @@
+import { EmbedBuilder } from "@discordjs/builders";
+
 import { client } from "./main.ts";
 export function handleEvent(gameEvent) {
   const channel = client.channels.cache.get("1293123587385069628");
   if (!channel) throw new Error("Invalid channel");
   if (gameEvent.event === "PlayerJoinEvent") {
-    channel.send(
-      `${gameEvent.player} joined the server!\nCurrent players online: ${gameEvent.playerCount}`
-    );
+    channel.send(getEmbed("join", gameEvent.player, gameEvent.playerCount));
   }
   if (gameEvent.event === "PlayerQuitEvent") {
-    channel.send(
-      `${gameEvent.player} left the server :(\nCurrent players online: ${gameEvent.playerCount}`
-    );
+    channel.send(getEmbed("leave", gameEvent.player, gameEvent.playerCount));
   }
+}
+
+function getEmbed(event, player, playerCount) {
+  const chatEmbed = new EmbedBuilder()
+    .setTitle(
+      event === "join"
+        ? `${player} joined the server!`
+        : `${player} left the server ):`
+    )
+    .setDescription(`*Current players online:* ${playerCount}`);
+  return { embeds: [chatEmbed] };
 }
