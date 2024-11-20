c816d43c4305d6eaa01a72a0873447136fa59fc1
Alexander Eide <atlexeide@gmail.com>
2024-11-20 15:07:30 +0100

    Move embed function

--- event-handler.ts
+++ event-handler.ts
@@ -1,31 +1,21 @@
-import { EmbedBuilder } from "@discordjs/builders";
 import dotenv from "dotenv";
 dotenv.config();
 import { client } from "./main.ts";
+import { getConnectionEmbed } from "./event_embeds/connection-embed.ts";
+
 export function handleEvent(gameEvent) {
   if (!process.env.DISCORD_LOG_CHANNEL_ID)
     throw new Error("No channel id in local enviroment");
   const channel = client.channels.cache.get(process.env.DISCORD_LOG_CHANNEL_ID);
   if (!channel || !channel.isSendable()) throw new Error("Invalid channel");
   if (gameEvent.event === "PlayerJoinEvent") {
-    channel.send(getEmbed("join", gameEvent.player, gameEvent.playerCount));
+    channel.send(
+      getConnectionEmbed("join", gameEvent.player, gameEvent.playerCount)
+    );
   }
   if (gameEvent.event === "PlayerQuitEvent") {
-    channel.send(getEmbed("leave", gameEvent.player, gameEvent.playerCount));
+    channel.send(
+      getConnectionEmbed("leave", gameEvent.player, gameEvent.playerCount)
+    );
   }
-}
-
-function getEmbed(event, player, playerCount) {
-  const chatEmbed = new EmbedBuilder()
-    .setAuthor({
-      name: player,
-      iconURL: `https://minotar.net/avatar/${player}.png`,
-    })
-    .setTitle(
-      event === "join"
-        ? `${player} joined the server!`
-        : `${player} left the server ):`
-    )
-    .setDescription(`*Current players online:* ${playerCount}`);
-  return { embeds: [chatEmbed] };
 }
