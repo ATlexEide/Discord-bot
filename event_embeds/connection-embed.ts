b7e08a81bf1faafba382424678c5c138786fffe6
Alexander Eide <atlexeide@gmail.com>
2024-11-20 18:24:32 +0100

    Fix object chaining

--- event_embeds/connection-embed.ts
+++ event_embeds/connection-embed.ts
@@ -1,12 +1,12 @@
 import { EmbedBuilder } from "@discordjs/builders";
 
-export function getConnectionEmbed(event, player, playerCount) {
+export function getConnectionEmbed(event) {
   const chatEmbed = new EmbedBuilder()
     .setAuthor({
-      name: player,
-      iconURL: `https://minotar.net/avatar/${player}.png`,
+      name: event.player,
+      iconURL: `https://minotar.net/avatar/${event.player}.png`,
     })
     .setTitle(event === "join" ? `joined the server!` : `left the server ):`)
-    .setDescription(`*Current players online:* ${playerCount}`);
+    .setDescription(`*Current players online:* ${event.playerCount}`);
   return { embeds: [chatEmbed] };
 }
