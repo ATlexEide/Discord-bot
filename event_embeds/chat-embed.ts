7bb77a2f79afff3f97c74a2fa7977004f346c8d7
Alexander Eide <atlexeide@gmail.com>
2024-11-21 17:13:09 +0100

    Update chat timestamp

--- event_embeds/chat-embed.ts
+++ event_embeds/chat-embed.ts
@@ -3,7 +3,8 @@ import { EmbedBuilder } from "@discordjs/builders";
 export function getChatEmbed(event) {
   const date = new Date();
   const hours = date.getHours();
-  const minutes = date.getMinutes();
+  const minutes =
+    date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
   console.log(`${hours}:${minutes}`);
   const chatEmbed = new EmbedBuilder().setAuthor({
     name: `[${hours}:${minutes}]  <${event.player}>  ${event.messsage}`,
