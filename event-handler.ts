d4809e4a0777a26515cab509226f8a4c2798f4d0
Alexander Eide <atlexeide@gmail.com>
2024-11-21 17:09:39 +0100

    Refactor event handler variable

--- event-handler.ts
+++ event-handler.ts
@@ -5,7 +5,7 @@ import { getConnectionEmbed } from "./event_embeds/connection-embed.ts";
 import { getGamemodeEmbed } from "./event_embeds/gamemode-embeds.ts";
 import { getServerStatusEmbed } from "./event_embeds/serverstatus-embed.ts";
 import { getChatEmbed } from "./event_embeds/chat-embed.ts";
-export function handleEvent(gameEvent) {
+export function handleEvent(gameData) {
   if (!process.env.DISCORD_LOG_CHANNEL_ID)
     throw new Error("No log channel id in local enviroment");
   if (!process.env.DISCORD_CHAT_CHANNEL_ID)
@@ -23,25 +23,25 @@ export function handleEvent(gameEvent) {
     !chatChannel.isSendable()
   )
     throw new Error("Invalid Channel");
-  console.log(gameEvent);
-  switch (gameEvent.event) {
+  console.log(gameData);
+  switch (gameData.event) {
     case "ServerStart":
-      logChannel.send(getServerStatusEmbed(gameEvent));
+      logChannel.send(getServerStatusEmbed(gameData));
       break;
     case "ServerStop":
-      logChannel.send(getServerStatusEmbed(gameEvent));
+      logChannel.send(getServerStatusEmbed(gameData));
       break;
     case "ChatEvent":
-      chatChannel.send(getChatEmbed(gameEvent));
+      chatChannel.send(getChatEmbed(gameData));
       break;
     case "PlayerJoinEvent":
-      logChannel.send(getConnectionEmbed(gameEvent));
+      logChannel.send(getConnectionEmbed(gameData));
       break;
     case "PlayerQuitEvent":
-      logChannel.send(getConnectionEmbed(gameEvent));
+      logChannel.send(getConnectionEmbed(gameData));
       break;
     case "PlayerGameModeChangeEvent":
-      logChannel.send(getGamemodeEmbed(gameEvent));
+      logChannel.send(getGamemodeEmbed(gameData));
       break;
 
     default:
