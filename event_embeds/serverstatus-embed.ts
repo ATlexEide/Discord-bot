196f3562fcdd471ee426d94306ee2e20bf73613e
Alexander Eide <atlexeide@gmail.com>
2025-05-03 04:29:42 +0200

    Fix notation

--- event_embeds/serverstatus-embed.ts
+++ event_embeds/serverstatus-embed.ts
@@ -1,22 +1,24 @@
 import { EmbedBuilder } from "discord.js";
 
 export function getServerStatusEmbed(event) {
+  console.clear();
+  console.log(event);
   const chatEmbed = new EmbedBuilder()
     .setAuthor({
       name: "Server",
       iconURL:
         event.event === "ServerStart"
           ? "https://i.imgur.com/wojm0r1.png"
-          : "https://i.imgur.com/E0VyRDS.png",
+          : "https://i.imgur.com/E0VyRDS.png"
     })
     .setTitle(
       event.event === "ServerStart" ? "Server started" : "Server stopped"
     );
   if (event.event === "ServerStart") {
     chatEmbed.setDescription(
-      `${event.motd}
+      `${event.server_message}
 
-      IP: *${event.ip}:${event.port}*`
+      IP: *${event.server_ip}:${event.server_port}*`
     );
   }
   return { embeds: [chatEmbed] };
