28220564794b318c5640d83fa011b7e7bba94d08
Alexander Eide <atlexeide@gmail.com>
2025-05-03 04:28:07 +0200

    Update return

--- event_embeds/chat-embed.ts
+++ event_embeds/chat-embed.ts
@@ -2,15 +2,16 @@ import { EmbedBuilder } from "@discordjs/builders";
 
 export function getChatEmbed(event) {
   const date = new Date();
-  const hours = date.getHours();
+  const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
   const minutes =
     date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
   console.log(`${hours}:${minutes}`);
   const chatEmbed = new EmbedBuilder().setAuthor({
     name: `[${hours}:${minutes}]  <${event.player}>  ${event.messsage}`,
-    iconURL: `https://minotar.net/avatar/${event.player}.png`,
+    iconURL: `https://minotar.net/avatar/${event.player}.png`
   });
   ///// Keep just in case i decide to use it later for whatever reason
   //   return { embeds: [chatEmbed] };
-  return `[${hours}:${minutes}]  <\\${event.player}>  ${event.messsage}`;
+  console.log("MESSAGE: ", event.message);
+  return `[${hours}:${minutes}]  <\\${event.player}>  ${event.message}`;
 }
