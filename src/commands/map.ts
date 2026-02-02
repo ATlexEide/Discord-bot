583e372149a41323ed59fc2fc16114c153314809
Alexander Eide <atlexeide@gmail.com>
2026-02-02 15:15:32 +0100

    Update logic

--- src/commands/map.ts
+++ src/commands/map.ts
@@ -1,17 +1,31 @@
 import dotenv from "dotenv";
 dotenv.config();
+// @ts-ignore
+import logo from "../woods.png";
 
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 
 export default {
   data: new SlashCommandBuilder()
     .setName("map")
-    .setDescription("show a tarkov map"),
+    .setDescription("show a tarkov map")
+    .addStringOption((Option) =>
+      Option.setName("map").setDescription("which map").setRequired(true)
+    ),
 
   async response(interaction: ChatInputCommandInteraction) {
-    switch ("map") {
-      case "map":
-        // files: [{ attachment: "YourImage.jpg" }];
+    if (!interaction.options.get("map")) return;
+
+    const map = interaction.options.get("map");
+    let fileURL: string;
+    console.log("///// MAP /////");
+    console.log(map?.value);
+    switch (map?.value) {
+      case "woods":
+        // fileURL = "./assets/images/woods.png";
+        interaction.reply({
+          files: [{ attachment: logo }]
+        });
         break;
 
       default:
