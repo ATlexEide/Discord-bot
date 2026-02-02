8b614191289c2e7d2e62755f82d87b1af7aaab9d
Alex <atlexeide@gmail.com>
2026-02-02 05:45:09 +0100

    Fix string

--- src/discord/embeds/listEmbed.ts
+++ src/discord/embeds/listEmbed.ts
@@ -2,11 +2,11 @@ import { EmbedBuilder } from "@discordjs/builders";
 
 export default function createListEmbed(title: string, listItems: any) {
   const test = listItems
-    .map((item: any) => `\n### ${item.name} \n ${item.description}\n`)
+    .map((item: any) => `\n### ${item.data.name} \n ${item.data.description}\n`)
     .join(" ");
   const embed = new EmbedBuilder()
     .setTitle(title)
     .setDescription(test.toString());
-
+  console.log(test);
   return embed;
 }
