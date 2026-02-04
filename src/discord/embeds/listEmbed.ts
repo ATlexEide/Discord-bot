045338722ff4fe2851927768e9a50b4c7d4aa16d
Alexander Eide <atlexeide@gmail.com>
2026-02-04 02:03:07 +0100

    Fix incorrect dot notation

--- src/discord/embeds/listEmbed.ts
+++ src/discord/embeds/listEmbed.ts
@@ -2,7 +2,10 @@ import { EmbedBuilder } from "@discordjs/builders";
 
 export default function createListEmbed(title: string, listItems: any) {
   const test = listItems
-    .map((item: any) => `\n### ${item.data.name} \n ${item.data.description}\n`)
+    .map(
+      (item: any) =>
+        `\n### ${item.command.data.name} \n ${item.command.data.description}\n`
+    )
     .join(" ");
   const embed = new EmbedBuilder()
     .setTitle(title)
