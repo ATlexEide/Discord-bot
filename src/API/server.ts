d2b42e1fdb16f69f5380938f17fb125268c0e9ad
Alexander Eide <atlexeide@gmail.com>
2026-02-05 19:45:26 +0100

    Update server logic

--- src/API/server.ts
+++ src/API/server.ts
@@ -1,11 +1,7 @@
 import fs from "node:fs";
 import path from "node:path";
 import express from "express";
-import {
-  handleEvent,
-  lastEvent,
-  serverStatus
-} from "../minecraft/event-handler.js";
+import { handleEvent } from "../minecraft/event-handler.js";
 
 let channels = {
   minecraft_server: {
@@ -15,54 +11,39 @@ let channels = {
   }
 };
 
-function fetchData() {
-  return fs.readFile(
-    path.resolve(import.meta.dirname, "./data/channels.json"),
-    function (err, data) {
-      if (err) throw err;
-      return JSON.parse(data.toString());
-    }
-  );
-}
-function updateData() {
-  fs.writeFile("api/data/channels.json", JSON.stringify(channels), () => {
-    fetchData();
-  });
-}
-
 export function startServer() {
   const port = 1337;
   const app = express();
   app.use(express.json());
 
   app.get("/channels", (req: any, res: any) => {
-    res.send(fetchData());
+    res.send();
   });
 
   app.get("/", (req: any, res: any) => {
-    res.send("Welcome to my server!");
+    res.json({ status: "OK", message: "yipp" });
   });
 
-  app.get("/test", (req: any, res: any) => {
+  app.post("/test", (req: any, res: any) => {
     res.send("This is a test yippieeee");
   });
 
   app.get("/events/last", (req: any, res: any) => {
-    res.send(lastEvent);
+    res.send("lastEvent");
   });
 
   app.get("/server/status", (req: any, res: any) => {
-    res.send(serverStatus);
+    res.send("serverStatus");
   });
 
-  app.post("/events", async (req: any, res: any) => {
+  app.post("/chat", async (req: any, res: any) => {
     console.clear();
     console.log(req.body);
     const event = await req.body;
-    handleEvent(event);
     console.log("Request recieved");
     console.log(event);
-    res.json({ status: "OK" });
+    handleEvent(event);
+    res.json({ status: "OK", message: "yipp" });
   });
 
   app.listen(port, () => {
