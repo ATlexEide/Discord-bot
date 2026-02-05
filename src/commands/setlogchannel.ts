ca2bdf0af2c9ae4664b4fbb3d1abac280272b1d8
Alexander Eide <atlexeide@gmail.com>
2026-02-05 12:33:10 +0100

    Add setter command for log channel

---
+++ src/commands/setlogchannel.ts
@@ -0,0 +1,56 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import {
+  ChatInputCommandInteraction,
+  SlashCommandBuilder,
+  MessageFlags
+} from "discord.js";
+import { db } from "../main.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("setlogchannel")
+    .setDescription("Sets this channel as the minecraft log channel"),
+
+  async response(interaction: ChatInputCommandInteraction) {
+    db.connect();
+    db.query(
+      `SELECT guildId FROM guilds WHERE guildId = ${interaction.guildId}`,
+      (error, result) => {
+        if (error) console.log(error);
+        // @ts-expect-error
+        if (!result[0]) {
+          console.log("DID NOT FIND GUILD\nCreating record...");
+          const query = `INSERT INTO guilds (guildId, log_channel_id)
+            VALUES (${interaction.guildId}, ${interaction.channelId})`;
+          db.query(query, function (error, results) {
+            if (error) throw error;
+            console.log("Created record: \n", results);
+
+            interaction.reply({
+              content: "Set this channel as the minecraft log channel",
+              flags: MessageFlags.Ephemeral
+            });
+          });
+        } else {
+          console.log("FOUND GUILD");
+          const query = `UPDATE guilds
+            SET log_channel_id = ${interaction.channelId}
+            WHERE guildId = ${interaction.guildId}`;
+          console.log("///// QUERY /////");
+          console.log(query);
+          db.query(query, function (error, results) {
+            if (error) throw error;
+            console.log("Updated record: \n", results);
+
+            interaction.reply({
+              content: "Set this channel as the minecraft log channel",
+              flags: MessageFlags.Ephemeral
+            });
+          });
+        }
+      }
+    );
+  }
+};
