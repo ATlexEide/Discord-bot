3694f179d559fafe92c8c93c7dfeaf1831b07680
Alexander Eide <atlexeide@gmail.com>
2026-03-23 00:57:41 +0100

    Add silly command

---
+++ src/commands/army.ts
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("army")
+    .setDescription("Velvet's army of children!"),
+
+  async response(interaction: ChatInputCommandInteraction) {
+    const res = emoji();
+    interaction.reply({
+      content: res
+    });
+  }
+};
+
+function emoji(emoji: string = "<:child420:1485309385361326221>") {
+  const charLimit = 2000;
+  console.log(emoji.length);
+  let emojiMax = Math.floor(charLimit / emoji.length);
+  console.log(charLimit);
+  console.log(emojiMax);
+  let res = "";
+  while (emojiMax > 0) {
+    res = res + emoji;
+    emojiMax--;
+  }
+  return res;
+}
