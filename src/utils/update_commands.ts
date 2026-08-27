e26f118b4b6f73cb9ce504e06b4ee32f87510996
Alexander Eide <atlexeide@gmail.com>
2026-08-27 17:42:07 +0200

    Update logic

--- src/utils/update_commands.ts
+++ src/utils/update_commands.ts
@@ -1,37 +1,43 @@
 import { Guild, REST, Routes } from "discord.js";
 import dotenv from "dotenv";
-import { cmdArr } from "../main.js";
+import { client, cmdArr } from "../main.js";
+
 dotenv.config();
 
 export async function refreshCommands(guild: Guild): Promise<boolean> {
+  console.log("Updating commands for:");
   if (!process.env.DISCORD_TOKEN) {
     throw new Error("You're being stupid, no token dumbass");
   }
 
   const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
-
   try {
     let _cmdArray: any = [];
     cmdArr.map((cmd) => {
       _cmdArray.push(cmd.data);
     });
 
-    const id = process.env.BOT_ID;
+    const id = client.user?.id;
     if (!id) throw new Error("No Bot id");
     if (!guild) throw new Error("No Guild");
 
-    await rest.put(Routes.applicationGuildCommands(id, guild.id /*GUILD ID*/), {
-      body: _cmdArray
-    });
-
-    await rest.put(Routes.applicationCommands(id), {
-      body: _cmdArray
-    });
-
-    return true;
+    return await rest
+      .put(Routes.applicationGuildCommands(id, guild.id), {
+        body: _cmdArray
+      })
+      .then((res) => {
+        console.log("res !!!!!", res);
+        return res ? true : false;
+      })
+      .catch((error) => {
+        return false;
+      });
+    // console.log(test);
+    // await rest.put(Routes.applicationCommands(id), {
+    //   body: _cmdArray
+    // });
   } catch (error: any) {
     console.log(error);
     throw new Error({ ...error });
   }
-  return false;
 }
