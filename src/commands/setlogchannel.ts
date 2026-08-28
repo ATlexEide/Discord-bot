4d49ed9a267dcd674e6e392a3dcdec33aaa5d0ba
Alex <atlexeide@gmail.com>
2026-08-28 11:31:05 +0200

    Fix sql calls

--- src/commands/setlogchannel.ts
+++ src/commands/setlogchannel.ts
@@ -1,3 +1,5 @@
+import { client, globalErrorHandler } from "../main.js";
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -7,8 +9,6 @@ import {
   MessageFlags,
   PermissionFlagsBits
 } from "discord.js";
-import { client } from "../main.js";
-import mysql from "mysql2";
 
 const name: string = "setlogchannel";
 export default {
@@ -18,18 +18,30 @@ export default {
     .setDescription("Sets this channel as the minecraft log channel"),
 
   async response(interaction: ChatInputCommandInteraction) {
-    const db = mysql.createConnection({
+    if (!interaction.guildId) {
+      interaction.reply({
+        content: "Failed to set log channel, no guild id",
+        flags: MessageFlags.Ephemeral
+      });
+      return;
+    }
+    const connection = await mysql.createConnection({
       host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE
     });
+    connection.connect();
+
     const user = await interaction.guild?.members.fetch(interaction.user.id);
     const userRoles = user?.roles.cache;
+
     if (!user) return;
     if (!userRoles) return;
+
     const hasMcAdmin = userRoles.get("1542467267500183552");
+
     if (!hasMcAdmin) {
       await interaction.reply({
         content: "You must be an mc admin to perform this action.",
@@ -37,52 +49,50 @@ export default {
       });
       return;
     }
-    db.connect();
-    db.query(
-      `SELECT guildId FROM guilds WHERE guildId = ${interaction.guildId}`,
-      (error, result) => {
-        if (error) console.log(error);
-        // @ts-expect-error
-        if (!result[0]) {
-          console.log("DID NOT FIND GUILD\nCreating record...");
-          const query = `INSERT INTO guilds (guildId, log_channel_id)
-            VALUES (${interaction.guildId}, ${interaction.channelId})`;
-          db.query(query, function (error, results) {
-            if (error) throw error;
-            console.log("Created record: \n", results);
 
-            interaction.reply({
-              content: "Set this channel as the minecraft log channel",
-              flags: MessageFlags.Ephemeral
-            });
-          });
-        } else {
-          console.log("FOUND GUILD");
+    //////
+    try {
+      const [res] = await connection.query(
+        `SELECT guildId FROM guilds WHERE guildId = ${interaction.guildId}`
+      );
 
-          const query = `UPDATE guilds
+      // @ts-ignore
+      console.log("RESULT YIPPIEEEE", res[0]);
+
+      // @ts-ignore
+      if (!res[0]) {
+        console.log("DID NOT FIND GUILD\nCreating record...");
+
+        const [res2] = await connection.query(`
+          INSERT INTO guilds (guildId, log_channel_id)
+          VALUES (${interaction.guildId}, ${interaction.channelId})
+          `);
+
+        console.log("RESS2222", res2);
+        console.log("Created record: \n", res2);
+      } else {
+        console.log("FOUND GUILD");
+
+        const [res3] = await connection.query(`
+            UPDATE guilds
             SET log_channel_id = ${interaction.channelId}
-            WHERE guildId = ${interaction.guildId}`;
-          console.log("///// QUERY /////");
-          console.log(query);
-
-          db.query(query, async function (error, results) {
-            if (error) throw error;
-            console.log("Updated record: \n", results);
-
-            interaction.reply({
-              content: "Set this channel as the minecraft log channel",
-              flags: MessageFlags.Ephemeral
-            });
-          });
-        }
-
-        if (interaction.guildId) {
-          const guild = client.guilds.cache.get(interaction.guildId);
-          const channel = guild?.channels.cache.get(interaction.channelId);
-          channel?.edit({ topic: "Minecraft log channel" });
-        }
+            WHERE guildId = ${interaction.guildId}
+            `);
+        console.log("Updated record: \n", res3);
       }
-    );
-    db.end();
+
+      const guild = client.guilds.cache.get(interaction.guildId);
+      const channel = guild?.channels.cache.get(interaction.channelId);
+      channel?.edit({ topic: "Minecraft log channel" });
+
+      interaction.reply({
+        content: "Set this channel as the minecraft log channel",
+        flags: MessageFlags.Ephemeral
+      });
+    } catch (error) {
+      globalErrorHandler(error);
+    }
+
+    connection.end();
   }
 };
