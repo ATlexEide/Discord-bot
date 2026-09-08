68063d267592b5b2914a44dba36eb1f9b5d3d84e
Alex <atlexeide@gmail.com>
2026-09-08 11:03:56 +0200

    .

--- src/API/server.ts
+++ src/API/server.ts
@@ -39,7 +39,7 @@ export function startServer() {
 
   app.use(
     cors({
-      origin: ["http://localhost:5173", "https://theburrow.no"]
+      origin: ["http://localhost:5173/events", "https://theburrow.no/events"]
     })
   );
 
