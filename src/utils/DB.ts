cf82d93b893ece9708e6c8b2c9beb28e5cf6c455
Alexander Eide <atlexeide@gmail.com>
2026-09-11 00:44:42 +0200

    Update db utils

--- src/utils/DB.ts
+++ src/utils/DB.ts
@@ -1,64 +1,12 @@
-import { QueryError, QueryResult } from "mysql2";
-import mysql from "mysql2";
+import { dbClient, globalErrorHandler } from "../main.js";
 
-export async function getChannel(gameData: any) {
-  console.log("//////////", "STEP 2:", "Calling getChannelOut", "//////////");
-  console.log(getChannelOut(gameData));
-}
-
-// TYPE: chat || log || whitelist
-export function TESTgetChannelOut(gameData: any) {
-  try {
-    const db = mysql.createConnection({
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE
-    });
-    let result;
-    db.query(
-      `SELECT ${gameData.eventType}_channel_id FROM guilds WHERE guildId = ${gameData.guild_id}`,
-      (err, res: QueryResult) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        // @ts-expect-error
-        result = Object.values(res[0])[0];
-
-        return result;
-      }
-    );
-  } catch (err) {
-    console.log(err);
-  }
-}
-// TYPE: chat || log || whitelist
-export function getChannelOut(gameData: any) {
-  try {
-    // TODO: Fix
-    // @ts-expect-error
-    const [r, f] = db.query(
-      `SELECT ${gameData.eventType}_channel_id FROM guilds WHERE guildId = ${gameData.guild_id}`,
-      (err: QueryError, res: QueryResult) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        // @ts-expect-error
-        if (!res[0]) {
-          console.error(
-            `couldnt find chat channel for guild id ${gameData.guild_id}`
-          );
-        }
-
-        console.log("channel");
-        // @ts-expect-error
-        return Object.values(res[0])[0];
-      }
-    );
-  } catch (err) {
-    console.log(err);
-  }
+export async function getChannelId(EventData: any) {
+  if (dbClient.closed) dbClient.reconnect();
+  const id = await dbClient
+    .execute(
+      `SELECT ${EventData.channel_type}_channel_id from guild_channels WHERE guild_id = ${EventData.guildId}`
+    )
+    .then((res) => res.rows[0][0])
+    .catch((e) => globalErrorHandler(e));
+  return id;
 }
