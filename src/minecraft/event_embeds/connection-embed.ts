e10827e156d0eb1c3e473bc194cebc25ecb3cd0d
Alexander Eide <atlexeide@gmail.com>
2026-09-11 00:44:14 +0200

    Update embed

--- src/minecraft/event_embeds/connection-embed.ts
+++ src/minecraft/event_embeds/connection-embed.ts
@@ -4,13 +4,13 @@ export function getConnectionEmbed(event: any) {
   const chatEmbed = new EmbedBuilder()
     .setAuthor({
       name: event.player,
-      iconURL: `https://minotar.net/avatar/${event.player}.png`
+      iconURL: `https://mineskin.eu/avatar/${event.player_uuid}`
     })
     .setTitle(
-      event.event === "PlayerJoinEvent"
+      event.name === "PlayerJoinEvent"
         ? `Joined the server!`
         : `Left the server ):`
     )
-    .setDescription(`*Current players online:* ${event.playerCount}`);
+    .setDescription(`*Current players online:* ${event.player_count}`);
   return { embeds: [chatEmbed] };
 }
