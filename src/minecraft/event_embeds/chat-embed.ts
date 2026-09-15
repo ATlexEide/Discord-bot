7873998dbc8479e8daa8b7ed0a7fefa8ac050db4
Alexander Eide <atlexeide@gmail.com>
2026-09-15 16:18:45 +0200

    .

--- src/minecraft/event_embeds/chat-embed.ts
+++ src/minecraft/event_embeds/chat-embed.ts
@@ -1,13 +1,15 @@
 export function getChatEmbed(event: any) {
   console.log(event);
-  // if (event.player.includes("§")) event.player = removeColorCode(event.player);
+  const username = event.player.includes("§")
+    ? removeColorCode(event.player)
+    : event.player;
 
   const date = new Date();
   const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
   const minutes =
     date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
 
-  return `[${hours}:${minutes}]  <${event.player}>  ${event.message}`;
+  return `[${hours}:${minutes}]  <${username}>  ${event.message}`;
 }
 
 function removeColorCode(name: any) {
