dcbf2883bedf40a87b358f5fb18bb31b7dbb498f
Alexander Eide <atlexeide@gmail.com>
2026-09-15 17:38:35 +0200

    bbbbb

--- src/API/server.ts
+++ src/API/server.ts
@@ -75,10 +75,11 @@ export function startServer() {
 
   app.get("/mc/status", async (req: any, res: any) => {
     try {
-      fetch(`https://${process.env.MC_SERVER_IP}/status`)
+      fetch(`http://${process.env.MC_SERVER_IP}/status`)
         .then((r) => r.json())
         .then((r) => res.send(r));
     } catch (e) {
+      res.send("FAILED");
       globalErrorHandler(e);
     }
   });
@@ -91,7 +92,7 @@ export function startServer() {
 export async function sendMcMessage(message: Message) {
   const bodyContent = `[Discord]<${message.author.displayName}> ${message.content}`;
 
-  const _message = await fetch(`https://${process.env.MC_SERVER_IP}/chat`, {
+  const _message = await fetch(`http://${process.env.MC_SERVER_IP}/chat`, {
     method: "POST",
     body: bodyContent
     // headers: { "Content-Type": "application/json" }
@@ -99,7 +100,7 @@ export async function sendMcMessage(message: Message) {
 }
 export function removeWhitelist(id: string, interaction: ButtonInteraction) {
   try {
-    fetch(`https://${process.env.MC_SERVER_IP}/whitelist/remove`, {
+    fetch(`http://${process.env.MC_SERVER_IP}/whitelist/remove`, {
       method: "POST",
       body: interaction.message.embeds[0].author?.name
     })
@@ -120,7 +121,7 @@ export async function whitelistPlayer(message: Message) {
       const author = message.author;
       switch (playerInfo.code) {
         case "player.found":
-          fetch(`https://${process.env.MC_SERVER_IP}/whitelist/add`, {
+          fetch(`http://${process.env.MC_SERVER_IP}/whitelist/add`, {
             method: "POST",
             body: `${message.author.displayName}|${playerInfo.data.player.username}`
           }).then((res) => {
@@ -188,6 +189,7 @@ async function handleServerEvent(event: any, res: any) {
     }
     res.send("OK");
   } catch (e) {
+    res.send("FAILED");
     globalErrorHandler(e);
   }
   function startTimer() {
