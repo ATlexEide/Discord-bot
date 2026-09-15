724eb79d8bd0c703f92f457035142b8abd1c912d
Alexander Eide <atlexeide@gmail.com>
2026-09-15 17:18:05 +0200

    a

--- src/minecraft/event_embeds/chat-embed.ts
+++ src/minecraft/event_embeds/chat-embed.ts
@@ -1,21 +1,16 @@
+import { globalErrorHandler } from "../../main.js";
+
 export function getChatEmbed(event: any) {
-  // console.log(event.player.includes("§"));
-  // console.log(typeof event.player);
-  // console.log(removeColorCode(event.player));
-  // const username = event.player.includes("§")
-  //   ? removeColorCode(event.player)
-  //   : event.player;
+  try {
+    const username = event.player;
+    const date = new Date();
+    const hours =
+      date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+    const minutes =
+      date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
 
-  const username = event.player;
-  const date = new Date();
-  const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
-  const minutes =
-    date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
-
-  return `[${hours}:${minutes}]  <${username}>  ${event.message}`;
+    return `[${hours}:${minutes}]  <${username}>  ${event.message}`;
+  } catch (e) {
+    globalErrorHandler(e);
+  }
 }
-
-// function removeColorCode(name: string) {
-//   let regex = /§(.)/g;
-//   return name.replace(regex, "");
-// }
